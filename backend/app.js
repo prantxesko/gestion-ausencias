83f65d2cd5b327b7642692b9921f76b8a01aa89b
xsaylor <xsaylor@hotmail.com>
2021-08-02 14:11:59 +0200

    Commit para casa

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,26 @@
+
+
 const express = require('express');
-const axios = require('axios');
+const firebase = require('firebase');
+const firestore = firebase.firestore();
+//const axios = require('axios');
 
 const app = express();
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+app.post('/licencia', (req, res)=>{
+  console.log(req.body)
+  res.end();
+})
+
+app.post('')
+
+
+app.listen(process.env.PORT || 8080, ()=>{
+  console.log(`App iniciada en puerto ${process.env.PORT || 8080}`)
+});
