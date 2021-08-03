598b9dd81a65b81c905cfaff7a3cfd85209d535c
xsaylor <xsaylor@hotmail.com>
2021-08-03 14:06:17 +0200

    Se independizan componente formulario y lista de licencias

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Licencias from './components/Licencias';
+import Licencias from './components/Licencias/Licencias';
 
 function App() {
   
--- cliente/src/components/Licencias/Licencias.jsx
+++ cliente/src/components/Licencias/Licencias.jsx
@@ -4,22 +4,31 @@ import axios from 'axios';
 const urlBase = 'http://localhost:8080/'; //Para modificar en producción;
 
 export default function Licencias() {
-  const inputsObject = {
-    descrip : '',
-    inicio : '',
-    fin : ''
-  }
-
-  const [inputs, setInputs] = useState(inputsObject);
+ 
   const [licencias, setLicencias] = useState([]);
 
   const getLicencias = async ()=>{
+    console.log('Obteniendo licencias')
+    return [
+      {
+        id: 1,  
+        descrip: 'HORAS 2020'
+      },
+      {
+        id: 2,  
+        descrip: 'HORAS 2021'
+      },
+      {
+        id: 3,  
+        descrip: 'VACACIONES 2020'
+      },
+    ];
     try{
       const response = await axios({
         method: "get",
         url: `${urlBase}licencias`,
         });
-      console.log('Response' ,response.data)
+      // console.log('Response' ,response.data)
       return response.data;
     }catch(error){
       console.log(error);
@@ -27,14 +36,42 @@ export default function Licencias() {
   }
 
   useEffect(()=>{
+ 
     getLicencias()
     .then(data => {
-      console.log('Data', data)
+      // console.log('Data', data)
       setLicencias(data)
     })
     .catch(error => console.log(error));
   }, [])
 
+ 
+
+
+  
+  return (
+    <div>
+      <LicenciasForm 
+        onSubmit = {
+          ()=> getLicencias()
+          .then(data => setLicencias(data))
+        }
+      />
+      <LicenciasLista licencias = {licencias} />
+    </div>
+   
+  )
+}
+
+
+function LicenciasForm(props){
+  const inputsObject = {
+    descrip : '',
+    inicio : '',
+    fin : ''
+  }
+
+  const [inputs, setInputs] = useState(inputsObject);
   const handleInputs = e =>{
     const newInputs = {...inputs};
     newInputs[e.target.name] = e.target.value;
@@ -52,28 +89,34 @@ export default function Licencias() {
     .catch(error => console.log('Error fatal', error));
     //console.log(inputsObject);
     setInputs(inputsObject);
-    setLicencias(await getLicencias());
+    props.onSubmit();
   }
+  return(
+    <form onSubmit= {handleSubmit} encType="multipart/form-data">
+        <input type="text" name = "descrip" placeholder  = "Descripción" required value ={inputs.descrip} onChange = {handleInputs}/>
+        <input type="date" name = "inicio" value ={inputs.inicio} required onChange = {handleInputs}/>
+        <input type="date" name = "fin" value ={inputs.fin} required onChange = {handleInputs}/>
+        <button type="submit"  >Crear Licencia</button>
+    </form>
+  )
+}
 
+
+function LicenciasLista(props){
+  const {licencias} = props;
+  
   const handleDelete = async e => {
     //console.log(e.target.id)
     await axios({
       method: "delete",
       url: `${urlBase}licencias?id=${e.target.id}`,
     })
+    // .then(props.onChange())
     .catch(error => console.log('Error fatal', error));
-    setLicencias(await getLicencias());
+   
   }
-  
-  return (
+  return(
     <div>
-      <form onSubmit= {handleSubmit} encType="multipart/form-data">
-        <input type="text" name = "descrip" placeholder  = "Descripción" required value ={inputs.descrip} onChange = {handleInputs}/>
-        <input type="date" name = "inicio" value ={inputs.inicio} required onChange = {handleInputs}/>
-        <input type="date" name = "fin" value ={inputs.fin} required onChange = {handleInputs}/>
-        <button type="submit"  >Crear Licencia</button>
-      </form>
-      <div>
         <h5>Lista de licencias vigentes</h5>
   
           {licencias.map(licencia =>(
@@ -84,15 +127,7 @@ export default function Licencias() {
             ) 
           )}
     
-      </div>
     </div>
-   
   )
 }
-
-
-function LicenciasForm(){
-  
-}
-
 
