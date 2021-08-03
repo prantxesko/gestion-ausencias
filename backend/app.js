cbeff870f859837fbb92b63b4882558ea6959da3
xsaylor <xsaylor@hotmail.com>
2021-08-03 11:52:15 +0200

    Funcinando creación y eliminación de licencias

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,72 @@
 
+//Inicio firebase y base de datos
 
-const express = require('express');
 const firebase = require('firebase');
-const firestore = firebase.firestore();
-//const axios = require('axios');
+require('firebase/firestore');
+const {firebaseConfig} = require('../firebase.js');
 
-const app = express();
-
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
-
-// Initialize Firebase
+//console.log(firebaseConfig)
 firebase.initializeApp(firebaseConfig);
 
-app.post('/licencia', (req, res)=>{
+const db = firebase.firestore();
+
+// Inicio aplicación
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const multer = require('multer');
+
+
+
+app.use(cors());
+app.use(multer().fields([]));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+
+app.post('/licencias', async (req, res) => {
   console.log(req.body)
+  try{
+    await db.collection('licencias').doc().set(req.body);
+  }catch(error){
+    console.log(error);
+  }
   res.end();
 })
 
-app.post('')
+app.post('/test', async (req, res) => {
+  //console.log(req)
+  res.end();
+})
+
+app.get('/licencias', async (req, res) => {
+  const query = db.collection('licencias');
+  const result = [];
+  try {
+    await query.get().then(querySnapshot => {
+      for (let doc of querySnapshot.docs) {
+        //console.log(`Found document at ${JSON.stringify(doc.data())}`);
+        result.push({id:doc.id, ...doc.data()});
+      };
+    })
+    res.send(result);
+  }catch(error){
+    res.status(500).send(error);
+  }
+ 
+});
+
+app.delete('/licencias', async (req, res) => {
+  try{
+    await db.collection('licencias').doc(req.query.id).set({eliminada: true}, {merge: true})
+    res.send(req.query.id);
+  }catch(error){
+    console.log('error')
+    res.status(500).send(error);
+  }
+})
 
 
 app.listen(process.env.PORT || 8080, ()=>{
--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import './App.css';
-import {firebase, firestore} from './firebase'
+import Licencias from './components/Licencias';
 
 function App() {
-  const [data, setData] = React.useState();
-  React.useEffect(() => {
-    const getData = async () =>{
-      const query = await firestore.collection('011').get();
-      query.forEach(item => console.log(item.id, item.data()));
-    }
-    getData();
-  });
+  
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      <Licencias/>
     </div>
   );
 }
