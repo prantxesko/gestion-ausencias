364c5be3c7a3bc906f8a1ea56598d788cafb1569
xsaylor <xsaylor@hotmail.com>
2021-08-10 14:10:12 +0200

    Commit para casa

--- backend/routes/operarios.js
+++ backend/routes/operarios.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const {db} = require('../firebase');
+
 
 router.get('/', (req, res) => {
   res.send('Ruta operarios')
-})
+});
+
+router.post('/:id', (req, res) =>{
+  console.log('eoo')
+  // try{
+    const idLicencia = req.query.licencia || '';
+    const idOperario = req.params.id;
+    console.log(req.params.id, idLicencia)
+    res.end();
+  // }catch(error){
+  //  return res.status(500).send
+  // }
+
+});
 
 module.exports = router;
